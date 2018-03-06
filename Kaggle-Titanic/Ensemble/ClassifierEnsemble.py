60b25032a6403033f7f961c3c88e4f52214f702e
Daniel Carter <danny_zz@hotmail.co.uk>
2018-03-06 23:49:59 +0000

    Got predictions and normalised datasets working

--- Kaggle-Titanic/Ensemble/ClassifierEnsemble.py
+++ Kaggle-Titanic/Ensemble/ClassifierEnsemble.py
@@ -55,14 +55,14 @@ class ClassifierEnsemble:
     ]
 
     # GridSearchCV parameter values will go in here, one set for each classifier
-    grid_n_estimator = [10, 50, 100, 300]
+    grid_n_estimator = [100, 200, 300, 400]
     grid_ratio = [.1, .25, .5, .75, 1.0]
     grid_learn = [.01, .03, .05, .1, .25]
     grid_max_depth = [2, 4, 6, 8, 10, None]
     grid_min_samples = [5, 10, .03, .05, .10]
     grid_criterion = ['gini', 'entropy']
     grid_bool = [True, False]
-    grid_seed = [0]
+    grid_seed = [111]
 
 
     parameterCandidates = [
@@ -70,7 +70,7 @@ class ClassifierEnsemble:
             # AdaBoostClassifier - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html
             'n_estimators': grid_n_estimator,  # default=50
             'learning_rate': grid_learn,  # default=1
-            # 'algorithm': ['SAMME', 'SAMME.R'], #default=’SAMME.R
+            'algorithm': ['SAMME', 'SAMME.R'], #default=’SAMME.R
             'random_state': grid_seed
         }],
 
@@ -92,9 +92,9 @@ class ClassifierEnsemble:
         [{
             # GradientBoostingClassifier - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingClassifier.html#sklearn.ensemble.GradientBoostingClassifier
             # 'loss': ['deviance', 'exponential'], #default=’deviance’
-            'learning_rate': [.05],
+            'learning_rate': grid_learn,
         # default=0.1 -- 12/31/17 set to reduce runtime -- The best parameter for GradientBoostingClassifier is {'learning_rate': 0.05, 'max_depth': 2, 'n_estimators': 300, 'random_state': 0} with a runtime of 264.45 seconds.
-            'n_estimators': [300],
+            'n_estimators': grid_n_estimator,
         # default=100 -- 12/31/17 set to reduce runtime -- The best parameter for GradientBoostingClassifier is {'learning_rate': 0.05, 'max_depth': 2, 'n_estimators': 300, 'random_state': 0} with a runtime of 264.45 seconds.
             # 'criterion': ['friedman_mse', 'mse', 'mae'], #default=”friedman_mse”
             'max_depth': grid_max_depth,  # default=3
@@ -108,6 +108,101 @@ class ClassifierEnsemble:
             'max_depth': grid_max_depth,  # default=None
             'oob_score': [True],
         # default=False -- 12/31/17 set to reduce runtime -- The best parameter for RandomForestClassifier is {'criterion': 'entropy', 'max_depth': 6, 'n_estimators': 100, 'oob_score': True, 'random_state': 0} with a runtime of 146.35 seconds.
+            'random_state': grid_seed
+        }],
+
+        [{
+            # GaussianProcessClassifier
+            'max_iter_predict': grid_n_estimator,  # default: 100
+            'random_state': grid_seed
+        }],
+
+        [{
+            # LogisticRegressionCV - http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegressionCV.html#sklearn.linear_model.LogisticRegressionCV
+            'fit_intercept': grid_bool,  # default: True
+            # 'penalty': ['l1','l2'],
+            'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga'],  # default: lbfgs
+            'random_state': grid_seed
+        }],
+
+        [{
+            # BernoulliNB - http://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.BernoulliNB.html#sklearn.naive_bayes.BernoulliNB
+            'alpha': grid_ratio,  # default: 1.0
+        }],
+
+        # GaussianNB -
+        [{}],
+
+        [{
+            # KNeighborsClassifier - http://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html#sklearn.neighbors.KNeighborsClassifier
+            'n_neighbors': [1, 2, 3, 4, 5, 6, 7],  # default: 5
+            'weights': ['uniform', 'distance'],  # default = ‘uniform’
+            'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+        }],
+
+        [{
+            # SVC - http://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
+            # http://blog.hackerearth.com/simple-tutorial-svm-parameter-tuning-python-r
+            # 'kernel': ['linear', 'poly', 'rbf', 'sigmoid'],
+            'C': [1, 2, 3, 4, 5],  # default=1.0
+            'gamma': grid_ratio,  # edfault: auto
+            'decision_function_shape': ['ovo', 'ovr'],  # default:ovr
+            'probability': [True],
+            'random_state': grid_seed
+        }]
+
+        # [{
+        #     # XGBClassifier - http://xgboost.readthedocs.io/en/latest/parameter.html
+        #     'learning_rate': grid_learn,  # default: .3
+        #     'max_depth': [1, 2, 4, 6, 8, 10],  # default 2
+        #     'n_estimators': grid_n_estimator,
+        #     'seed': grid_seed
+        # }]
+    ]
+
+    presetParameters = [
+        [{
+            # AdaBoostClassifier - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html
+            'n_estimators': grid_n_estimator,  # default=50
+            'learning_rate': grid_learn,  # default=1
+            'algorithm': ['SAMME', 'SAMME.R'],  # default=’SAMME.R
+            'random_state': grid_seed
+        }],
+
+        [{
+            # BaggingClassifier - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.BaggingClassifier.html#sklearn.ensemble.BaggingClassifier
+            'n_estimators': grid_n_estimator,  # default=10
+            'max_samples': grid_ratio,  # default=1.0
+            'random_state': grid_seed
+        }],
+
+        [{
+            # ExtraTreesClassifier - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.ExtraTreesClassifier.html#sklearn.ensemble.ExtraTreesClassifier
+            'n_estimators': grid_n_estimator,  # default=10
+            'criterion': grid_criterion,  # default=”gini”
+            'max_depth': grid_max_depth,  # default=None
+            'random_state': grid_seed
+        }],
+
+        [{
+            # GradientBoostingClassifier - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.GradientBoostingClassifier.html#sklearn.ensemble.GradientBoostingClassifier
+            # 'loss': ['deviance', 'exponential'], #default=’deviance’
+            'learning_rate': grid_learn,
+            # default=0.1 -- 12/31/17 set to reduce runtime -- The best parameter for GradientBoostingClassifier is {'learning_rate': 0.05, 'max_depth': 2, 'n_estimators': 300, 'random_state': 0} with a runtime of 264.45 seconds.
+            'n_estimators': grid_n_estimator,
+            # default=100 -- 12/31/17 set to reduce runtime -- The best parameter for GradientBoostingClassifier is {'learning_rate': 0.05, 'max_depth': 2, 'n_estimators': 300, 'random_state': 0} with a runtime of 264.45 seconds.
+            # 'criterion': ['friedman_mse', 'mse', 'mae'], #default=”friedman_mse”
+            'max_depth': grid_max_depth,  # default=3
+            'random_state': grid_seed
+        }],
+
+        [{
+            # RandomForestClassifier - http://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier
+            'n_estimators': grid_n_estimator,  # default=10
+            'criterion': grid_criterion,  # default=”gini”
+            'max_depth': grid_max_depth,  # default=None
+            'oob_score': [True],
+            # default=False -- 12/31/17 set to reduce runtime -- The best parameter for RandomForestClassifier is {'criterion': 'entropy', 'max_depth': 6, 'n_estimators': 100, 'oob_score': True, 'random_state': 0} with a runtime of 146.35 seconds.
             'random_state': grid_seed
         }],
 
@@ -190,10 +285,10 @@ class ClassifierEnsemble:
 
     def trainAllClassifiers(self, trainingDataFrame, predictorColumns, labelColumn):
 
-        #self.calculate_weights(trainingDataFrame, labelColumn)
+        self.calculate_weights(trainingDataFrame, labelColumn)
 
         trainingInputs = trainingDataFrame[predictorColumns]
-        trainingInputs = preprocessing.normalize(trainingInputs, axis=0)
+        #trainingInputs = preprocessing.normalize(trainingInputs, axis=0)
 
         trainingLabels = trainingDataFrame[labelColumn]
 
@@ -203,10 +298,17 @@ class ClassifierEnsemble:
 
         messages = []
 
+        normal_names = ["SVC", "MLPClassifier", "NearestNeighbors", "SGDClassifier"]
+
         for algo, parameters in zip(self.MLA, self.parameterCandidates):
 
-            # if algo[0] == "etc" or algo[0] == "rfc" or algo[0] == "lr" or algo[0] == "svc":
-            #     parameters[0]["class_weight"] = [{1: self.one_weight}]
+            inputs = trainingInputs
+
+            if type(algo).__name__ in normal_names:
+                inputs = preprocessing.normalize(trainingInputs, axis=0)
+
+            if algo[0] == "etc" or algo[0] == "rfc" or algo[0] == "lr" or algo[0] == "svc":
+                parameters[0]["class_weight"] = [{1: self.one_weight}]
             #
             # print(parameters)
             # print("")
@@ -220,7 +322,7 @@ class ClassifierEnsemble:
                 n_jobs=-1)
 
 
-            best_search.fit(trainingInputs, trainingLabels)
+            best_search.fit(inputs, trainingLabels)
             run = time.perf_counter() - start
 
             best_param = best_search.best_params_
@@ -281,9 +383,9 @@ class ClassifierEnsemble:
 
     def getAllClassifierPredictions(self, testDataFrame, predictorColumns, labelColumn):
         print("doSomething")
+        normal_names = ["SVC", "MLPClassifier", "NearestNeighbors", "SGDClassifier"]
 
         inputPredictors = testDataFrame[predictorColumns]
-        inputPredictors = preprocessing.normalize(inputPredictors, axis=0)
 
         inputLabels = testDataFrame[labelColumn]
 
@@ -291,8 +393,14 @@ class ClassifierEnsemble:
 
         for classifier in self.fitClassifiers:
 
-            model_preds = classifier.predict(inputPredictors)
+            inputs = inputPredictors
+
+            if type(classifier).__name__ in normal_names:
+                inputs = preprocessing.normalize(inputPredictors, axis=0)
+
+            model_preds = classifier.predict(inputs)
             #predictions_df.append(model_preds[0])
+            print(type(classifier).__name__)
             print(model_preds)
             print()
 
